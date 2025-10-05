027dbcb42ee1398e347a2e0b497a9a609b425c6d
Umesh Pradip Sanap <umesh2809@Umeshs-MacBook-Air.local>
2025-10-06 00:00:23 +0530

    second commit new changes

--- SMSApp/admin.py
+++ SMSApp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import StudentManagement
 # Register your models here.
+admin.site.register(StudentManagement)
--- SMSApp/models.py
+++ SMSApp/models.py
@@ -11,10 +11,16 @@ class StudentManagement(models.Model):
         ('E','E-Division'),
         ('F','F-Division')
     )
+    std_Class = (
+        ('1','1 class'),
+        ('2','2 class'),
+        ('3','3 class'),
+        ('4','4 class')
+    )
     std_name = models.CharField(max_length=50)
     std_roll_no = models.IntegerField()
-    std_division = models.CharField(max_length=10)
-    std_standard = models.CharField(max_length=1, choices=Std_Grade)
+    std_division = models.CharField(max_length=1,  choices=Std_Grade)
+    std_standard = models.CharField(max_length=1, choices=std_Class)
     std_email = models.EmailField(unique=True)
     std_address = models.CharField(max_length=50)
 
